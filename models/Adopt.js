c9d06444556b4dbf8f2b8f74afed2a76f7d4d5c6
Christian Ares <christianaresvillan@gmail.com>
2020-11-24 12:28:51 +0100

    Changes

--- models/Adopt.js
+++ models/Adopt.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const adoptSchema = new Schema({
     Name: {type: String},
     breed: {type: String},
-    birthDate: {type: Date},
+    birthDate: {type: String},
     Gender: {type: String},
     //Image: {type: Image}
 })
